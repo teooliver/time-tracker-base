9a677f507cdd521a45b7644034368b5694cb257b
Teo Oliver <teo.liz.oli@gmail.com>
2020-12-31 17:34:03 +0000

    fix createTask not returning full object from database

---
+++ client/src/interfaces/task.ts
@@ -0,0 +1,9 @@
+export interface ITask {
+  _id: string;
+  name: string;
+  timeInSeconds: number;
+  initialTime: Date;
+  endTime: Date;
+  createdAt: Date;
+  updatedAt: Date;
+}
--- server/controllers/tasks.ts
+++ server/controllers/tasks.ts
@@ -8,19 +8,20 @@ export const getTasks = async (req: Request, res: Response) => {
 
     res.status(200).json(tasks);
   } catch (error) {
-    res.status(404).json({ message: error.message });
+    res.status(404).json(error);
   }
 };
 
-export const createTask = (req: Request, res: Response) => {
+export const createTask = async (req: Request, res: Response) => {
   const task = req.body;
 
   const newTask = new Task(task);
 
   try {
-    newTask.save();
-    res.status(201).json(newTask);
+    const savedTask = await newTask.save();
+
+    res.status(201).json(savedTask);
   } catch (error) {
-    res.status(409).json({ message: error.message });
+    res.status(409).json(error);
   }
 };
