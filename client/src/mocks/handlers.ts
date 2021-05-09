c932bfc79ccbe66e3cb4c34e483c4cd293e7a4e0
Teo Oliver <teo.liz.oli@gmail.com>
2021-05-09 19:30:29 +0100

    add useGroupedTasks mock handler and mock data

--- client/src/mocks/handlers.ts
+++ client/src/mocks/handlers.ts
@@ -1,6 +1,7 @@
 import { rest } from 'msw';
 import { API_URL } from '../utils/api-client';
 import { getClientRes } from './data/getClientsRes';
+import { getGroupedTasksRes } from './data/getGroupedTasksRes';
 import { getProjectsRes } from './data/getProjectsRes';
 
 export const handlers = [
@@ -11,11 +12,20 @@ export const handlers = [
       ctx.json(getProjectsRes)
     );
   }),
+
   rest.get(`${API_URL}/clients`, (req, res, ctx) => {
     return res(
       ctx.status(200),
 
       ctx.json(getClientRes)
+    );
+  }),
+
+  rest.get(`${API_URL}/tasks/group`, (req, res, ctx) => {
+    return res(
+      ctx.status(200),
+
+      ctx.json(getGroupedTasksRes)
     );
   }),
 ];
