68bfa7ee33df8f5cae4f988138ff9cd815f7edbb
Teo Oliver <teo.liz.oli@gmail.com>
2021-05-16 00:39:49 +0100

    scss and storybook now working

--- server/server.ts
+++ server/server.ts
@@ -1,23 +1,29 @@
-import express from "express";
-import bodyParser from "body-parser";
-import mongoose from "mongoose";
-import cors from "cors";
-import taskRoutes from "./routes/tasks";
-import projectRoutes from "./routes/projects";
-import clientRoutes from "./routes/clients";
-import seedRoutes from "./routes/seeds";
-import config from "./config/config";
+import express from 'express';
+import bodyParser from 'body-parser';
+import mongoose from 'mongoose';
+import cors from 'cors';
+import taskRoutes from './routes/tasks';
+import projectRoutes from './routes/projects';
+import clientRoutes from './routes/clients';
+import seedRoutes from './routes/seeds';
+import config from './config/config';
 
 const app = express();
 
 //  extended: "true"
-app.use(bodyParser.json({ limit: "30mb" }));
-app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
-app.use(cors());
-app.use("/tasks", taskRoutes);
-app.use("/projects", projectRoutes);
-app.use("/clients", clientRoutes);
-app.use("/seed", seedRoutes);
+app.use(bodyParser.json({ limit: '30mb' }));
+app.use(bodyParser.urlencoded({ limit: '30mb', extended: true }));
+app.use(
+  cors({
+    origin: '*',
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    preflightContinue: false,
+  })
+);
+app.use('/tasks', taskRoutes);
+app.use('/projects', projectRoutes);
+app.use('/clients', clientRoutes);
+app.use('/seed', seedRoutes);
 
 // Connect to Mongo
 mongoose
@@ -32,4 +38,4 @@ mongoose
   )
   .catch((error) => console.log(error.message));
 
-mongoose.set("useFindAndModify", false);
+mongoose.set('useFindAndModify', false);
