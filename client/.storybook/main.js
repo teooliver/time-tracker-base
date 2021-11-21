868f91c7e85bde9c15a8d5b90cde43ecaf7888ab
Teo Oliver <teo.liz.oli@gmail.com>
2021-11-21 20:25:03 +0000

    update storybook config

--- client/.storybook/main.js
+++ client/.storybook/main.js
@@ -24,26 +24,4 @@ module.exports = {
     // Return the altered config
     return config;
   },
-  // module: {
-  //   rules: [
-  //     {
-  //       test: /\.scss$/,
-  //       use: [
-  //         {
-  //           loader: 'style-loader',
-  //         },
-  //         {
-  //           loader: 'css-loader',
-  //         },
-  //         {
-  //           loader: 'sass-loader',
-  //           options: {
-  //             javascriptEnabled: true,
-  //           },
-  //         },
-  //       ],
-  //       include: path.resolve(__dirname, '../'),
-  //     },
-  //   ],
-  // },
 };
